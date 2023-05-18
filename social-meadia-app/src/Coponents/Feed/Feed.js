79ff959ceaaa6c180e2e9f27fc2a2e68557d0356
Rinooj N R <rinoojnr@gmail.com>
2023-05-18 15:07:35 +0530

    Modified feed Container

--- social-meadia-app/src/Coponents/Feed/Feed.js
+++ social-meadia-app/src/Coponents/Feed/Feed.js
@@ -1,10 +1,122 @@
 import React from 'react'
 import './Feed.css'
+import { MdRssFeed } from 'react-icons/md'
+import { IoSchoolOutline } from 'react-icons/io5'
+import person from '../../Static/person.jpeg'
 
 const Feed = () => {
   return (
     <div className='feed-container'>
-        Feed
+      <div className='feed-wrapper'>
+        <ul className='sidebar-list'>
+          <li className='sidebar-listitem'>
+            <MdRssFeed className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+          <li className='sidebar-listitem'>
+            <MdRssFeed className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+          <li className='sidebar-listitem'>
+            <MdRssFeed className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+          <li className='sidebar-listitem'>
+            <MdRssFeed className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+          <li className='sidebar-listitem'>
+            <MdRssFeed className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+          <li className='sidebar-listitem'>
+            <IoSchoolOutline className='sidebaricons'/>
+            <span className='sidebarlistname'>Feed</span>
+          </li>
+        </ul>
+        <button className='sidebarbutton'>View More</button>
+        <hr className='sidebarline'/>
+        <ul className='sidebarfriendlist'>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+          <li className='sidebarfrienditem'>
+            <img className='sidebarfriend' src={person} alt=''/>
+            <span className='sidebarname'>Joel</span>
+          </li>
+         
+         
+        </ul>
+      </div>
     </div>
   )
 }
