83b0cf9e1e1d60f37e486f6543296720bfb2ebcc
Rinooj N R <rinoojnr@gmail.com>
2023-05-18 16:41:34 +0530

    Added Center Feed Container

--- social-meadia-app/src/Coponents/Feed/Centerfeed.js
+++ social-meadia-app/src/Coponents/Feed/Centerfeed.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Centerfeed.css'
-
+import person from '../../Static/person.jpeg'
 const Centerfeed = () => {
   return (
     <div className='center-feed-container'>
-        Centerfeed
+        <div className='centerfeed-wrapper'>
+        <img className='centerbarfriend' src={person} alt=''/>
+        <input placeholder='What is in your mind...?' className='centersearchtext'/>
+        <hr className='shareHr'/>
+        </div>
+        {/* <div className='post-container'>
+        <img className='centerbarfriend' src={person} alt=''/>
+        </div> */}
+
     </div>
   )
 }
